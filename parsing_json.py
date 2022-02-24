68933e7b5aa350e9f3beecce450e970242ffed9f
zckrywlsn <zckry.wlsn@gmail.com>
2022-02-24 06:26:40 -0800

    first json parse

---
+++ parsing_json.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""Parsing structured JSON text into native Python data structures...
+
+...and how to access and work with nested data.
+"""
+
+
+import json
+import os
+from pprint import pprint
+
+
+# Get the absolute path for the directory where this file is located "here"
+here = os.path.abspath(os.path.dirname(__file__))
+print("os.path.dirname ", os.path.dirname(__file__))
+print("here ", here)
+print("os.path.join ",os.path.join(here, "interface-config.json"))
+# Read in the JSON text
+with open(os.path.join(here, "interface-config.json")) as file:
+    json_text = file.read()
+
+
+# Display the type and contents of the json_text variable
+print("json_text is a", type(json_text))
+print(json_text)
+
+
+# Use the json module to parse the JSON string into native Python data
+json_data = json.loads(json_text)
+
+
+# Display the type and contents of the json_data variable
+print("json_data is a", type(json_data))
+pprint(json_data)
+print("pprint json_data....")
+pprint(json_data["ietf-interfaces:interface"]["ietf-ip:ipv4"]["address"][0]["ip"])
